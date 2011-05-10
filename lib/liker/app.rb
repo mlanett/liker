328cc33cd118673cf159f7648e0e974b9e92c3dd
Mark Lanett <mark.lanett@gmail.com>
2011-05-09 17:56:11 -0700

    Move views into their own files

--- lib/liker/app.rb
+++ lib/liker/app.rb
@@ -14,10 +14,8 @@ module Liker
     use Clogger, :format => '$status "$request" ($request_time)', :logger => $stdout, :reentrant => true
     
     configure do
-      set :app_id,            development? && LIKER_ML || LIKER_B
-      set :app_file,          __FILE__ # or could set public and views
-      set :inline_templates,  true
-      set :public,            "public"
+      set :public,            File.expand_path("../../../public", __FILE__)
+      set :views,             File.expand_path("../views", __FILE__)
       
       @@photos = {}
       pubdir = File.expand_path("../../../public", __FILE__)
@@ -38,73 +36,31 @@ module Liker
     end
     
     helpers do
-      def fbconnect( title, image )
-        @this_app_id = settings.development? && LIKER_ML || LIKER_B
-        @this_domain = request.host
-        @like_url    = request.url
-        @like_title  = title
-        @like_image  = image
-      end
+      # @see http://developers.facebook.com/docs/opengraph/
+      # Should have title, type, image, url, site_name
+      @@graph_default = {
+        :fb_admins    => "1664736154",
+        :fb_app_id    => settings.development? ? LIKER_ML : LIKER_B,
+        #og_image     => "http://www.example.com/logo.gif",
+        :og_site_name => "Liker",
+        :og_title     => "Example",
+        :og_type      => "product",
+        :og_url       => "http://www.example.com/"
+      }
     end
     
     get "/" do
-      haml :index, :locals => { :title => "Liker", :photos => @@photos }, :layout => false
+      @graph = @@graph_default.merge :og_url => request.url, :og_title => "Liker", :og_type => "website", :site => request.host
+      haml :index, :locals => { :photos => @@photos }
     end
 
     get "/:name" do
       name = params[:name]
       if file = @@photos[name] then
-        fbconnect( name, url(file) )
-        haml :page, :locals => { :title => name, :file => file }
+        @graph = @@graph_default.merge :og_url => request.url, :og_title => name, :og_image => url(file), :site => request.host
+        haml :page, :locals => { :file => file }
       end
     end  
     
   end # App
 end # Liker
-
-__END__
-
-@@ layout
-%html{ :xmlns => "http://www.w3.org/1999/xhtml", :"xmlns:og" => "http://ogp.me/ns#", :"xmlns:fb" => "http://www.facebook.com/2008/fbml" }
-  %head
-    %title= title
-    - if @like_url then
-      %meta{ :property => "og:title",     :content => @like_title }
-      %meta{ :property => "og:type",      :content => "blog" }
-      %meta{ :property => "og:url",       :content => @like_url }
-      %meta{ :property => "og:image",     :content => @like_image }
-      %meta{ :property => "og:site_name", :content => "Liker" }
-      %meta{ :property => "fb:app_id",    :content => @this_app_id }
-      %meta{ :property => "fb:admins",    :content => "1664736154" }
-  %body
-    %div#wrapper
-      = yield
-
-@@ index
-- photos.each_pair do |name,path|
-  %div
-    %a{ :href => ("/#{name}") }= name
-
-@@ page
-%link{ :rel => "stylesheet", :href => "/table.css", :type => "text/css" }
-%div#header
-  %h1.title= title
-%table{ :cellspacing => 0, :cellpadding => 0, :border => 0 }
-  %tr
-    %td.content
-      %div.photo
-        %img{ :src => ("/#{file}"), :"max-width" => "640", :"max-height" => "640" }
-    %td.menu
-      %fb:like{ :href => @like_url, :show_faces => true, :width => 300, :send => 1 }
-      %br
-      %br
-      %fb:recommendations{ :site => @this_domain, :width => 300, :height => 300, :header => "true" }
-      %br
-      %br
-      %fb:activity{ :site => @this_domain, :width => "300", :height => "300", :header => "true", :recommendations => "false" }
-      %br
-      %br
-      %fb:comments{ :href => @like_url, :num_posts => 2, :width => 300 }
-%div#footer
-%div#fb-root
-%script{ :src => "http://connect.facebook.net/en_US/all.js#appId=199890130028792&amp;xfbml=1" }
